4ddc074f2d27391a477c4f12b766a2d13dcede2a
seyonr <seyonr@seyonrs-MacBook-Pro.local>
2024-06-04 12:44:48 -0400

    update for grammer

--- js/main.js
+++ js/main.js
@@ -17,3 +17,28 @@ function galleryScroll(){
     const shop_scroll = document.querySelector(".collage-div");
     shop_scroll.scrollIntoView({ behavior: 'smooth' });
 }
+
+// Fade Animation
+document.addEventListener('DOMContentLoaded', () => {
+    const faders = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0.1,
+        rootMargin: "0px 0px -100px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('visible');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+});
